5effd2519408dd6261542cbfe4cb026951ba9ca6
DenzelFoxUA <canemidnight21@gmail.com>
2024-09-15 19:45:37 +0300

    Adding weekends and days till Birthday calculator

--- C#BasicsChallengeProject/BirthdateCalculatorChallenge/BirthdateCalculator.cs
+++ C#BasicsChallengeProject/BirthdateCalculatorChallenge/BirthdateCalculator.cs
@@ -10,14 +10,18 @@ namespace C_BasicsChallengeProject.BirthdateCalculatorChallenge
     {
         public const int DAYS_PER_YEAR = 365;
         public const int MONTHS_PER_YEAR = 12;
-
+        public const int DAYS_PER_WEEK = 7;
+        public const int WEEKENDS_PER_WEEK = 2;
         public static void Run()
         {
-            DateTime birthdate = new DateTime(1989,01,21);
+            DateTime birthdate = new DateTime(1989, 01, 21);
             Console.WriteLine(CalculateAgeInDays(birthdate));
             Console.WriteLine(CalculateAgeInYears(birthdate));
             Console.WriteLine(CalculateAgeInMonths(birthdate));
-            
+            Console.WriteLine(CalaculateDaysToNextBirthday(birthdate));
+            Console.WriteLine(CalaculateDaysToNextBirthday(new DateTime(1986, 12, 28)));
+            Console.WriteLine(WeekendsTillBirthday(birthdate));
+
         }
 
         public static int CalculateAgeInDays(DateTime birthdate)
@@ -33,7 +37,7 @@ namespace C_BasicsChallengeProject.BirthdateCalculatorChallenge
 
             int monthsPassedAfterBirthDayThisYear;
 
-            if(isDatePassed == true)
+            if (isDatePassed == true)
             {
                 monthsPassedAfterBirthDayThisYear = DateTime.Now.Date.Month - birthdate.Date.Month;
             }
@@ -51,9 +55,92 @@ namespace C_BasicsChallengeProject.BirthdateCalculatorChallenge
             return (int)(CalculateAgeInDays(birthdate) / DAYS_PER_YEAR);
         }
 
+        public static int CalaculateDaysToNextBirthday(DateTime birthdate)
+        {
+            bool dayIsPassed = isBirthdayPassedThisYear(birthdate);
+            int thisYear = DateTime.Now.Year,
+                nextYear = thisYear + 1,
+                endOfThisYearIndex = new DateTime(thisYear, 12, 31).DayOfYear,
+                birthDayThisYearIndex = new DateTime(thisYear, birthdate.Month, birthdate.Day).DayOfYear,
+                birthdayNextYearIndex = new DateTime(nextYear, birthdate.Month, birthdate.Day).DayOfYear,
+                todayIndex = DateTime.Today.Date.DayOfYear;
+
+            if (dayIsPassed == true)
+            {
+                int daysToEndOfAYear = endOfThisYearIndex - todayIndex;
+                return daysToEndOfAYear + birthdayNextYearIndex;
+            }
+            else
+            {
+                return birthDayThisYearIndex - todayIndex;
+            }
+        }
+
+        public static int WeekendsTillBirthday(DateTime birthdate)
+        {
+            int daysToNextWeekend = DateTime.Today.GetDaysToNextWeekend();
+
+            int totalDaysLeft = CalaculateDaysToNextBirthday(birthdate);
+            
+            int todaySaturday = DateTime.Today.IsTodaySaturday() ? 1 : 0;
+            
+
+            double preciseNumbersOfWeekends 
+                = (totalDaysLeft - daysToNextWeekend) / (double)DAYS_PER_WEEK * (double)WEEKENDS_PER_WEEK + todaySaturday;
+            
+            return (int)preciseNumbersOfWeekends;        
+        }
+
         private static bool isDatePassedThisMonth(int date)
         {
             return date <= DateTime.Now.Date.Day;
         }
+
+        private static bool isBirthdayPassedThisYear(DateTime birthdate)
+        {
+            DateTime today = DateTime.Now.Date;
+
+            return new DateTime(today.Year,birthdate.Month,birthdate.Day) <= today.Date;
+        }
+
+        private static int GetDaysToNextWeekend(this DateTime value)
+        {
+            var day = value.DayOfWeek;
+
+            switch(day)
+            {
+                case DayOfWeek.Monday: return 5;
+                case DayOfWeek.Tuesday: return 4;
+                case DayOfWeek.Wednesday: return 3;
+                case DayOfWeek.Thursday: return 2;
+                case DayOfWeek.Friday: return 1;
+                case DayOfWeek.Saturday: return 0;
+                case DayOfWeek.Sunday: return 0;
+                default: break;
+            }
+
+            return 0;
+        }
+
+        private static bool IsTodaySunday()
+        {
+            return DateTime.Today.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        private static bool IsTodaySunday(this DateTime value)
+        {
+            return value.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        private static bool IsTodaySaturday()
+        {
+            return DateTime.Today.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+        private static bool IsTodaySaturday(this DateTime value)
+        {
+            return value.DayOfWeek == DayOfWeek.Saturday;
+        }
+
     }
 }
